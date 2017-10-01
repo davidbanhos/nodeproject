8df67f646eaea7400228be64f9f708dcecd1aec0
David Banhos <david.banhos@gmail.com>
2017-10-01 04:54:24 +0000

    Add bower - bootstrap and jquery.

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,24 @@
-var aloha = 'Aloha variables';
-console.log(aloha  + ' ' + new Date());
+var express = require('express');
+var app = express();
+
+//Cloud9 por assignment
+var port = process.env.PORT;
+
+//Static
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+//Routing
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+//Routing
+app.get('/routing', function(req, res){
+    res.send('Hello World! - ROUTING');
+});
+
+app.listen(port,function(err){
+   console.log('The server is running on port: ' + port); 
+});
