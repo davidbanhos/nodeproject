e5ba2db185bd6850fea96fe735dee0495a992595
David Banhos <david.banhos@gmail.com>
2017-10-02 02:46:02 +0000

    gulp and nodemon

--- nodeproject/gulpfile.js
+++ nodeproject/gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
- 
+var nodemon = require('gulp-nodemon');
 var jsFiles = ['*.js','src/**/*.js']; 
  
 gulp.task('style', function(){
@@ -13,6 +13,16 @@ gulp.task('default', function(){
 });
 
 gulp.task('inject', function() {
+
+    var inject = require('gulp-inject');
+    var injectSrc = gulp.src(['./public/css/*.css'
+                        ,'!./public/css/*min*'                 
+                        ,'./public/js/*.js'
+                        ,'!./public/js/agency.js']);
+    var injectOptions = {
+        ignorePath: '/public'          
+    };
+
     var wiredep = require('wiredep').stream;
     var options = {
         bowerJson: require('./bower.json'),
@@ -22,6 +32,18 @@ gulp.task('inject', function() {
     
     return gulp.src('./src/views/*.html')
                 .pipe(wiredep(options))
+                .pipe(inject(injectSrc, injectOptions))
                 .pipe(gulp.dest('./src/views'));
 });
 
+gulp.task('serve', ['style','inject'], function(){
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        watch: jsFiles
+    };
+    
+    return nodemon(options).on('restart', function(ev) {
+        console.log('Restarting server ...');
+    });
+});
